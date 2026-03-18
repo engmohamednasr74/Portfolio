fe45954161f73ffd54753f6b798462c7142736ee
engmohamednasr74 <eng.mohamed.nasr.74@gmail.com>
2026-03-18 19:55:40 +0200

    وصف التعديل

---
+++ js/loading.js
@@ -0,0 +1,52 @@
+gsap.fromTo(
+  ".loading-page",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    duration: 1.5,
+    delay: 3.5,
+    display: "none",
+  },
+);
+
+gsap.fromTo(
+  ".logo-name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 1.5,
+    delay: 0.5,
+  },
+);
+
+gsap.fromTo(
+  ".particles,.hero,.services,.projects,.certifications,.contact",
+  {
+    opacity: 0,
+    // display: "none",
+  },
+  {
+    opacity: 1,
+    duration: 1.5,
+    delay: 4,
+    // display: "block",
+  },
+);
+
+gsap.fromTo(
+  ".navbar,.modal-overlay,.success-modal,.footer",
+  {
+    opacity: 0,
+    // display: "none",
+  },
+  {
+    opacity: 1,
+    duration: 1.5,
+    delay: 4,
+    // display: "flex",
+  },
+);
